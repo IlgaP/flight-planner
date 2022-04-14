8f5fcacf4266f5b19ccc4e5192a701fdbb669b09
IlgaP <91965275+IlgaP@users.noreply.github.com>
2022-04-14 10:47:59 +0300

    security, 00 test

---
+++ src/main/java/io/codelex/flightplanner/security/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package io.codelex.flightplanner.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers().permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
